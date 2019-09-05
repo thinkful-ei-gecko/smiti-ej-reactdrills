baa7a6a3e95659a02737ee7ab2d8b5e935269492
EJ <ejg132@gmail.com>
2019-09-05 11:37:24 -0700

    fixed bomb

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -6,48 +6,20 @@ class Bomb extends Component {
         count: 0
     }
 
+
     componentDidMount(){
-<<<<<<< HEAD
-
-        this.setState({
-            count: this.state.count + 1
-        })
-
-        let interval = setInterval(timer(), 1000);
-
-        function timer() {
-            
-
-            console.log(count);
-            if (count % 2 === 0){
-                return count = 'tick';
-            } else if (count % 2 !== 0){
-                return count = 'tock'
-            } else if (count >= 8) {
-                return count = 'BOOM!!!'
-            } else {
-                return this.state.setState({
-                    count: this.state.count + 1
-                })
-            }
-        }
-
-=======
         this.interval = setInterval(() => {
             // eslint-disable-next-line
             this.setState({
                 count: this.state.count + 1
             })
-            
+
         }, 1000)
     }
-
-    componentWillUnmount(){
+    
+    componentWillUnmount() {
         clearInterval(this.interval)
     }
-
-
-
 
     display()
     {
@@ -55,36 +27,23 @@ class Bomb extends Component {
         if (count >= 8){
             clearInterval(this.interval)
             return 'BOOM!!!'
-        }
-        else if(count % 2 === 0)
-        {
+        } else if(count % 2 === 0) {
             return 'tick'
-        }
-        else
-        {
+        } else if(count % 2 !== 0) { 
             return 'tock'
         }
-        
-        
->>>>>>> fa710154ca5969dd71a8d4d45fabf86be55878a4
     }
+    
 
     render(){
 
         return(
             <div>
-<<<<<<< HEAD
-=======
                 <p>{this.display()}</p>
->>>>>>> fa710154ca5969dd71a8d4d45fabf86be55878a4
                 <p>{this.state.count}</p>
             </div>
         )
     };
-
-    componentWillUnmount(){
-        clearInterval(this.interval);
-    }
 }
 
 export default Bomb;
